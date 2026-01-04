10c576d10c95508f5dc8a3102d56fb78ccde8580
Tarique Ansari <tariqueansari1020@gmail.com>
2026-01-04 14:25:22 +0530

    Delete SpringBoot/spring-boot-crud-with-mysql-database directory

--- SpringBoot/spring-boot-crud-with-mysql-database/src/main/java/com/hcl/spring_boot_crud_with_mysql_database/mapping/one_to_one/repository/AdharRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.spring_boot_crud_with_mysql_database.mapping.one_to_one.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.spring_boot_crud_with_mysql_database.mapping.one_to_one.entity.Adhar;
-
-public interface AdharRepository extends JpaRepository<Adhar, Long> {
-
-}
